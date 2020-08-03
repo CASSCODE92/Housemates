fe7433da49a5cd7187598b5b5b94fd727aff5b08
CASSCODE92 <cassie2012abioye@gmail.com>
2020-08-02 22:31:51 -0400

    const event

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { response } from 'express';
 
 function App() {
   return (
@@ -32,6 +33,27 @@ const app= express();
 
 app.get("/", function (req, res){
   res.send("server is up & running.")
+
+  response.on("data", function(data){
+    const event= {
+      
+
+
+
+    }
+    // console.log(JSON.stringify(event));
+
+
+
+  }
+  
+  
+  
+  
+  
+  
+  
+  )
 
 
   app.listen(3004, function(){
